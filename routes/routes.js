8ecce850056e03772e624955b7d5ebb9b22285bc
Zeyu-Li <zeyu.li23@gmail.com>
2021-08-12 09:17:58 -0600

    working add product

--- routes/routes.js
+++ routes/routes.js
@@ -18,5 +18,18 @@ router.patch("/", async (req, res) => {
     res.send("Patch")
 })
 
+// adds a product
+router.post("/products", async (req, res) => {
+    const product = new Product({
+        product_id: req.body.product_id
+    })
+
+    product.save().then(result => {
+        res.status(200).json({message: "Product added successfully"})
+    }).catch(err => {
+        res.status(400).json({message: err.message})
+    })
+})
+
 module.exports = router
 // export {router}
